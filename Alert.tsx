07fd2a5cfb52f7e0e8ff9afeb567c0e0daaeb9e0
Brian Castillo <157941280+briankerv@users.noreply.github.com>
2024-12-16 16:42:50 +0800

    Add files via upload

---
+++ Alert.tsx
@@ -0,0 +1,19 @@
+import React, { ReactNode } from "react";
+
+interface Props {
+    children: ReactNode; 
+    onClose: () => void; // Prop to handle close action
+}
+
+const Alert = ({ children, onClose }: Props) => {
+    return (
+        <div className="alert alert-primary">
+            {children}
+            <button type="button" className="close" onClick={onClose}>
+                &times; {/* Close button (X) */}
+            </button>
+        </div>
+    );
+}
+
+export default Alert;
